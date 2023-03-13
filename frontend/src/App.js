4948295fa676a189e3ad4a595c2bab4971c6609e
mahmoudfat7i <devmahmoudfathy@gmail.com>
2023-03-13 22:18:08 +0200

    react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+    <h1>WElcome To MERN app </h1>
+    </>
+  );
+}
+//function App() {
+//  return (
+//    <>
+//    <h1>WElcome To MERN app </h1>
+//    </>
+//  );
+//}
+
+export default App;
