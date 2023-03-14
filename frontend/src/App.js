e65fd9a9162aba3ac173e933e5421e646b8a3e45
mahmoudfat7i <devmahmoudfathy@gmail.com>
2023-03-14 23:16:32 +0200

    frontupdate1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './comonents/Header';
+import Footer from './comonents/Footer';
+import HomeScreen from './screens/HomeScreen';
 const App = () => {
   return (
     <>
-    <h1>WElcome To MERN app </h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 //function App() {
 //  return (
 //    <>
---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'chair 1',
+    image: '/images/airpods.png',
+    description:
+      'Introducing our premium ergonomic chair - the perfect addition to any modern workspace. Our chair is designed for optimal comfort and support, with a sleek and stylish design that will elevate any office or home environment.With its modern and minimalist design,With its modern and minimalist design,With its modern and minimalist design,',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4
+  },
+  {
+    _id: '2',
+    name: 'chair 2',
+    image: '/images/phone.png',
+    description:
+      'Crafted from high-quality materials, our chair features a breathable mesh backrest that promotes good posture and reduces heat buildup, while the contoured seat cushion provides all-day comfort. The adjustable lumbar support helps to reduce lower back pain and can be customized to fit your unique needs.With its modern and minimalist design,With its modern and ',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4
+  },
+  {
+    _id: '3',
+    name: 'chair 3 ',
+    image: '/images/camera.png',
+    description:
+      'Our chair also includes fully adjustable armrests that can be raised, lowered, angled, and moved forward or backward to provide maximum support for your arms and shoulders. The chair can be easily adjusted to suit your height and preferences, with a smooth and quiet 360-degree swivel and tilt mechanism that allows you to effortlessly recline and relax during long work sessions.',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3
+  },
+  {
+    _id: '4',
+    name: 'chair 4',
+    image: '/images/playstation.png',
+    description:
+      ' our chair is the perfect combination of style and comfort, making it an excellent choice for anyone looking to upgrade their workspace. Whether you are a busy professional, a gamer, or a student, our chair is sure to meet your needs and exceed your expectations. Try it today and experience the ultimate in ergonomic comfort!With its modern and ',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3
+  },
+  {
+    _id: '5',
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.png',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2
+  },
+  {
+    _id: '6',
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.png',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4
+  }
+];
+
+export default products;
